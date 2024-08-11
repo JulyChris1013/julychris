8c29ed3e80472448176225539df189b5ba4d92f1
JulyChris1310# <bodajuliychris@gmail.com>
2024-08-11 19:47:16 -0300

    fixed main tiltes

--- src/app/carousel/main-carousel.tsx
+++ src/app/carousel/main-carousel.tsx
@@ -8,8 +8,8 @@ export default function MainCarousel() {
 
       <div style={{ width: "100%", height: "auto", position: "absolute", bottom: "1em", padding: "1em" }}>
         <div style={{ width: "fit-content", textAlign: "center" }}>
-          <h1 style={{ fontSize: "calc(3vw + 1.5vw)" }}>¡Nos Casamos!</h1>
-          <h2 style={{ fontWeight: "200", fontSize:"calc(0.5vw + 1.5vw)" }}>y queremos compartirlo con vos</h2>
+          <h1 className="title-main-image">¡Nos Casamos!</h1>
+          <h2 className="subtitle-main-image">y queremos compartirlo con vos</h2>
         </div>
       </div>
     </div>
